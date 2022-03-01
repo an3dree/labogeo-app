a89b0a7ebb282774cfcfaf75cecbad012363b073
André Luiz <an3dree@gmail.com>
2022-03-01 11:31:04 -0300

    first pages layouts and contents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './components/contato/contato.component';
+import { HomeComponent } from './components/home/home.component';
+import { MembrosComponent } from './components/membros/membros.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'membros', component: MembrosComponent},
+  { path: 'projetos', component: ProjetosComponent},
+  { path: 'sobre', component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { BsDropdownModule, BsDropdownConfig, BsDropdownDirective } from 'ngx-bootstrap/dropdown';
+import { MembrosComponent } from './components/membros/membros.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    CarouselComponent,
+    MembrosComponent,
+    ProjetosComponent,
+    ContatoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    { provide: BsDropdownConfig, useValue: { autoClose: true }},
+    { provide: BsDropdownDirective }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
